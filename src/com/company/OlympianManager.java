b3f4ca83e14aa08402c3ba6e1d9d5d3f2bd21f48
Arthur Pearson <Arthur.Pearson1@marist.edu>
2014-10-26 17:24:18 -0400

    Start of Proj3

--- src/com/company/OlympianManager.java
+++ src/com/company/OlympianManager.java
@@ -3,103 +3,113 @@ package com.company;
 // Base class that is called from Main for Olympians
 public class OlympianManager
 {
-    private String[] name;
-    private Olympian.Sex[] sex;
-    private int[] age;
-
+    private static Olympian[] myOlympian = new Olympian[16];
     public OlympianManager()
     {
-        this.name = Olympian.name;
-        this.sex = Olympian.sex;
-        this.age = Olympian.age;
-    }
+        for(int i=0; i< myOlympian.length; i++)
+        {
+            myOlympian[i].name = "Smith";
+            myOlympian[i].sex = Olympian.Sex.MALE;
+            myOlympian[i].age = 0;
+        }
 
-    public Olympian getOlympians()
+    }
+    public void setOlympians()
     {
-        for (int i = 0; i < Olympian.name.length; i++)
+        for (int i = 0; i < 16; i++)
         {
             switch (i)
             {
                 case 0:
-                    Olympian.name[i] = "Joe";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 15;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 1:
-                    Olympian.name[i] = "Ann";
-                    Olympian.sex[i] = Olympian.Sex.FEMALE;
-                    Olympian.age[i] = 22;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 2:
-                    Olympian.name[i] = "Joe";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 15;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 3:
-                    Olympian.name[i] = "Jonathan";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 31;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 4:
-                    Olympian.name[i] = "Jarred";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 23;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 5:
-                    Olympian.name[i] = "Brian";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 31;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 6:
-                    Olympian.name[i] = "Debbie";
-                    Olympian.sex[i] = Olympian.Sex.FEMALE;
-                    Olympian.age[i] = 55;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 7:
-                    Olympian.name[i] = "Quentin";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 41;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 8:
-                    Olympian.name[i] = "Randy";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 90;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 9:
-                    Olympian.name[i] = "Howard";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 41;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 10:
-                    Olympian.name[i] = "Josie";
-                    Olympian.sex[i] = Olympian.Sex.FEMALE;
-                    Olympian.age[i] = 22;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 11:
-                    Olympian.name[i] = "Abby";
-                    Olympian.sex[i] = Olympian.Sex.FEMALE;
-                    Olympian.age[i] = 10;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 12:
-                    Olympian.name[i] = "Amanda";
-                    Olympian.sex[i] = Olympian.Sex.FEMALE;
-                    Olympian.age[i] = 15;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 13:
-                    Olympian.name[i] = "Chris";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 14;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 case 14:
-                    Olympian.name[i] = "Aaron";
-                    Olympian.sex[i] = Olympian.Sex.MALE;
-                    Olympian.age[i] = 23;
+                    myOlympian[i].name = "Bob";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
+                    break;
+                case 15:
+                    myOlympian[i].name = "Bob15";
+                    myOlympian[i].sex = Olympian.Sex.MALE;
+                    myOlympian[i].age = 15;
                     break;
                 default:
                     break;
             }
         }
-        return new Olympian();
+    }
+
+    public Olympian[] getOlympians()
+    {
+        return myOlympian;
     }
 }
+
 
